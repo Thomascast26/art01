5a014653d47e4c94ce1b4b88d170cd93c5751d57
Thomascast26 <114198757+Thomascast26@users.noreply.github.com>
2022-10-12 17:52:57 -0700

    Linking

---
+++ lab4/js/lab.js
@@ -0,0 +1,40 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+
+//
+// 		Task 1: Pseudocode an Everyday Task
+// 		Pseudocoding Gardening
+// 			Find a place to garden.
+// 			Find a spot with good soil.
+// 			Gather seeds, garden tools, and fertilizer.
+// 		Gather water.
+// 			Prepare the soil.
+//
+// 				Remove weeds
+// 				Dig holes for plants.
+// 				Spread fertilizer.
+//
+// 			Plant the seeds
+//
+// 				Water the seeds.
+// 				Cover the seeds with soil.
+// 				Wait for plant to grow.
+//
+//
+// 		Task 2: Pseudocode a Computer Task
+// 		Pseudocoding a Hangman Game
+//
+// 			Print game board
+// 			Accept user input
+// 			Check user input against hidden solution
+//
+// 				If user input is correct, reveal solution
+// 				Else print additional part of hangman
+//
+// 					If hangman is fully drawn, user loses
+// 					Else print additional hangman part
+//
