7cf946bbc2b8dc3ecec5beb6a765c8c8c0d9b156
Thomascast26 <114198757+Thomascast26@users.noreply.github.com>
2022-10-14 19:15:36 -0700

    Lab 5 jVS

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/**
+ * Author:    Max and Thomas
+ * Created:   10.14.2022
+ **/
+
+// defines all the variables
+ carMake = "Honda";
+ carYear = 2008;
+ carModel = "Civic";
+ carColor = "Blue";
+
+// age calculation
+var age = 2022 - carYear;
+
+// output
+
+document.writeln("Make: " + carMake + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + age + " years<br>");
