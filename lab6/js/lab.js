db0560e30bd34b5b6dbc5a690b25b1808f9120e9
Thomascast26 <114198757+Thomascast26@users.noreply.github.com>
2022-10-23 21:51:22 -0700

    Fix LAb 7 file

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -4,7 +4,6 @@
  *
  * (c) Copyright by Public Domain
  **/
-console.log("hello world")
 
  myTransport = ["Car", "Bike", "Carpool", "Bus"];
 
@@ -17,6 +16,7 @@ console.log("hello world")
          return 2022 - age;
      }
  }
+
  document.writeln("My" + myTransport + "<br>");
  document.writeln("My Main Ride: <pre>",
      JSON.stringify(myMainRide, null, '\t'), "</pre>");
