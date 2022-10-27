cb473752a42112d1e357797e9ebb95c86ea2433f
Thomascast26 <114198757+Thomascast26@users.noreply.github.com>
2022-10-26 23:21:36 -0700

    lab 8

---
+++ lab8/js/lab.js
@@ -0,0 +1,29 @@
+/**
+ * Author:    Max and Thomas
+ * Created:   10.26.2022
+ * License: Public Domain
+ **/
+
+
+  arr = [72, 5, 64, 9, 11, 2]
+
+  function checkEven(x) {
+      return (x % 2 == 0);
+  };
+
+  // checkEven tests
+  console.log("Test will pass 'true' if 2 is even: " + checkEven(2));
+
+  // My Array
+  console.log("My array: " + arr);
+
+  // checking the dispersal of the function
+  var result = arr.map(checkEven);
+  console.log("This shows the dispersal of the function over the array: " + result);
+
+  var results = arr.map(function(x){
+      var results = x ** 0.5
+      return results
+  });
+
+  console.log("Square root of all array values: " + results);
