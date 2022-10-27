d61456433ee865cb5f755b918e77e291c6ac8798
Thomascast26 <114198757+Thomascast26@users.noreply.github.com>
2022-10-26 23:30:00 -0700

    Lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -5,25 +5,25 @@
  **/
 
 
-  arr = [72, 5, 64, 9, 11, 2]
+ arr = [72, 5, 64, 9, 11, 2]
 
-  function checkEven(x) {
-      return (x % 2 == 0);
-  };
+ function checkEven(x) {
+     return (x % 2 == 0);
+ };
 
-  // checkEven tests
-  console.log("Test will pass 'true' if 2 is even: " + checkEven(2));
+ // checkEven tests
+ console.log("Test will pass 'true' if 2 is even: " + checkEven(2));
 
-  // My Array
-  console.log("My array: " + arr);
+ // My Array
+ console.log("My array: " + arr);
 
-  // checking the dispersal of the function
-  var result = arr.map(checkEven);
-  console.log("This shows the dispersal of the function over the array: " + result);
+ // checking the dispersal of the function
+ var result = arr.map(checkEven);
+ console.log("This shows the dispersal of the function over the array: " + result);
 
-  var results = arr.map(function(x){
-      var results = x ** 0.5
-      return results
-  });
+ var results = arr.map(function(x){
+     var results = x ** 0.5
+     return results
+ });
 
-  console.log("Square root of all array values: " + results);
+ console.log("Square root of all array values: " + results);
